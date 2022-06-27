17dab18fbdf75aa522538815f05db9a4b085f8cd
Jhony <scostajhony@gmail.com>
2022-06-27 12:05:04 -0300

    Adicionando exercicio ponteiro

---
+++ ponteiro/soma.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+ /*Escreva uma função soma que recebe um ponteiro de inteiro
+ num e mais dois inteiros a e b. A função deve calcular a 
+ soma de a+b em num. */
+
+
+
+ void soma(int* num, int a, int b){
+    *num = a + b;
+ }
+
+
